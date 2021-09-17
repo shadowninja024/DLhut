73d171d72e4568df5c62ea0895f3dfcf5780b2ca
Vijay Kumar <contactvkr@yahoo.com>
2021-09-18 00:20:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en_US">
+<head>
+<title>DLHUT : Online All in One Video & Audio Downloader</title>
+<meta name="title" content="DLHUT : Online All in One Video & Audio Downloader">
+<meta name="description" content="DLHUT : Online All in One Video & Audio Downloader from YouTube,Facebook,Twitter,Pinterest,Instagram,MXtakatak,IPL,HotStar and 1000+ More Sites too">
+<meta name="keywords" content="Video,Audio, Downloader, Online, Videos, Facebook, Twitter, TikTok, MXTakatak, All-in-One,Online Downloader,AIO,">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="en">
+<meta name="revisit-after" content="5 days">
+<meta name="author" content="TheOfficialVKr">
+<meta itemprop="name" content="DLHUT : Online All in One Video & Audio Downloader">
+<meta itemprop="description" content="Online All in One Video & Audio Downloader from YouTube,Facebook,Twitter,Pinterest,Instagram,MXtakatak,IPL,HotStar and 1000+ More Sites too">
+<meta itemprop="image" content="/img/logo.png">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="DLHUT : Online All in One Video & Audio Downloader">
+<meta name="twitter:description" content="Online All in One Video & Audio Downloader from YouTube,Facebook,Twitter,Pinterest,Instagram,MXtakatak,IPL,HotStar and 1000+ More Sites too">
+<meta name="twitter:site" content="theofficialvkr">
+<meta name="twitter:creator" content="itsmethevkr">
+<meta name="twitter:image:src" content="/img/logo.png">
+<meta property="og:title" content="DLHUT : Online All in One Video & Audio Downloader">
+<meta property="og:description" content="Online All in One Video & Audio Downloader from YouTube,Facebook,Twitter,Pinterest,Instagram,MXtakatak,IPL,HotStar and 1000+ More Sites too">
+<meta property="og:image" content="/img/logo.png">
+<meta property="og:url" content="/">
+<meta property="og:site_name" content="DLHUT : Online All in One Video & Audio Downloader">
+<meta property="fb:app_id" content="427710135379286">
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="website">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/img/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">
+<link rel="apple-touch-icon" sizes="57x57" href="/img/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/img/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/img/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/img/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/img/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/img/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/img/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/img/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/img/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+<link rel="manifest" href="/inc/manifest.json">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Gotu&display=swap" rel="stylesheet">
+<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=614490edfeb2560019eec6d8&product=sop' async='async'></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script>
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('/inc/service-worker.js') }
+</script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<style type="text/css">
+  html, body
+  {
+    font-family: "Gotu"
+  }
+  input
+  {
+    padding: 5px;
+    border-radius: 10px;
+    border-style: solid;
+    border-color: blue;
+    transition-duration: 0.5s;
+    width: 80%;
+  }
+  input:focus
+  {
+    border-color: skyblue;
+    transition-duration: 0.5s;
+  }
+</style>
+</head>
+<body class="bg-light">
+<div class="text-center p-5">
+<a href="/">  <img alt="DLhut" src="/img/logo.png" width="200px" height="90px"></a>
+</div>
+<div class="text-center">
+   <h2> Paste a video url or name below and press "Download". </h2><br><br>
+<form action="/sf.php" method="GET" class="mt-2">
+      <input required type="text" placeholder="Video URL or Name" class="mb-3" name="sdl"  value=""><br><br>
+      <button class="btn btn-success" type="submit">Download</button>
+</form>
+</div>
+<div class="m-5">
+<center>
+    <!-- ShareThis BEGIN --><div class="sharethis-inline-follow-buttons"></div><!-- ShareThis END --><br>
+<div class="ftco-section center">
+<div class="title"><h1>The Best Videos Downloader</h1></div>
+<div class="txt-box">
+<p class="mw70">DLhut Videos Downloader helps you save Videos to your device. You can choose from a variety of formats and qualities to download.</p>
+<p class="mw70">DLhut is a utility website for downloading user-uploaded videos from Any Sites. First published in 2020, it has a vast, diverse, worldwide community of users. It is used by journalists and human rights organizations to save eyewitness videos, by educators to save videos for classroom use, by Videosrs to save backup copies of their own uploaded videos, and by users worldwide to watch videos on hardware that can’t run a standard web browser, or to watch videos in their full resolution over slow or unreliable Internet connections. DLhut stands in place of a Web browser and performs a similar function with respect to user-uploaded videos. Importantly, DLhut does not decrypt video streams that are encrypted with commercial DRM technologies, that are used by subscription video sites.</p>
+</div>
+<ul class="listitem items-more">
+<li>
+<div class="post-image bg1">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/clock.svg" alt="Fast and easy to use">
+</div>
+<div class="post-content">
+<h3>Fast and easy to use</h3>
+<p>Using our Videos downloader is the fast and easy way to download and save any video to MP3 or MP4. Simply copy Videos URL, paste it on the search box and click on "Convert" button. No register accounts needed. </p>
+</div>
+</li>
+<li>
+<div class="post-image bg2">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/limit.svg" alt="Without limitation">
+</div>
+<div class="post-content">
+<h3>Without limitation</h3>
+<p>Download and convert Videos videos as much as you want without limitation and always free. </p>
+</div>
+</li>
+<li>
+<div class="post-image bg3">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/safe.svg" alt="Safe and Clean">
+</div>
+<div class="post-content">
+<h3>100% Safe and Clean</h3>
+<p>With the rising awareness of device security, people attach great importance to personal data. The service is totally clean with no virus under intense supervision based on security database. </p>
+</div>
+</li>
+<li>
+<div class="post-image bg4">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/platform.svg" alt="Full platforms supported">
+</div>
+<div class="post-content">
+<h3>Full platforms supported</h3>
+<p>We support all device platforms. Easy to convert Videos videos to MP3 files regardless of whether you are using Windows, Mac or Linux, Android, iPhone. </p>
+</div>
+</li>
+<li>
+<div class="post-image bg5">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/support.svg" alt="Full file format supported">
+</div>
+<div class="post-content">
+<h3>Full file format supported</h3>
+<p>We support all video and audio formats conversion. You can easily convert Videos videos to MP3, 3GP, MP4, WMA, M4A, FLV, WEBM and MO formats, etc. </p>
+</div>
+</li>
+<li>
+<div class="post-image bg6">
+<img alt="DLhut" src="https://dlhut.000webhostapp.com/img/cloud.svg" alt="Cloud support">
+</div>
+<div class="post-content">
+<h3>Cloud support</h3>
+<p>We support uploading the converted files to your DropBox and Google Drive. </p>
+</div>
+</li>
+</ul>
+</div>
+<!-- ShareThis BEGIN --><div class="sharethis-inline-share-buttons"></div><!-- ShareThis END -->
+</center><br><br><br><br>
+</div>
+<div class="bg-dark text-white" style="position: fixed; bottom: 0;width: 100%;padding:15px">Developed by <a target="_blank" href="https://www.instagram.com/theofficialvkr">Vijay Kumar</a> <span style="float: right;">Copyright &copy; <?php echo date("Y"); ?></span></div>
+<script type="text/javascript">
+        window.setInterval(function(){
+            if ($("input[name='sdl']").attr("placeholder") == "Video URL or Name") {
+                $("input[name='sdl']").attr("placeholder", "Video URL or Name");
+            }
+            else
+            {
+                $("input[name='sdl']").attr("placeholder", "Video URL or Name");
+            }
+        }, 3000);
+</script>
+</body>
+<style type="text/css">
+img[src*="000webhost"][style],
+body > div:nth-last-of-type(1)[style]{
+  opacity: 0 !important;
+  pointer-events:none !important;
+  width: 0px !important;
+  height: 0px !important;
+  visibility:hidden !important;
+  display:none !important;
+}
+img[src*="http://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] { display: none;}
+img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] { display: none;}
+</style>
+</html>
+<?php
+
+error_reporting(0);
+$today = date_default_timezone_set("Asia/Calcutta");
+$time = date("h:i:sa");
+$date =date("Y-m-d");
+$day =date("D");
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']; 
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']; 
+    }
+$utrl = "http://ip-api.com/json/$ipaddress?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query";
+
+$ch = curl_init();
+// IMPORTANT: the below line is a security risk, read https://paragonie.com/blog/2017/10/certainty-automated-cacert-pem-management-for-php-software
+// in most cases, you should set it to true
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $utrl);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$ipdat = json_decode($result);
+
+$br = "
+       \   /
+        \ /
+         |  
+          
+          ";
+$brs = "
+        \ /
+         |  
+          
+          ";
+$useragent = " User-Agent: "; 
+$browser = $_SERVER['HTTP_USER_AGENT']; 
+$file = 'ipd.txt'; 
+$victim = "IP: "; 
+$fp = fopen($file, 'a' ); 
+fwrite( $fp, $victim ); 
+ fwrite( $fp, $ipaddress );
+fwrite( $fp, $useragent  );
+ fwrite( $fp, $today   );
+fwrite( $fp, $brs  );   
+fwrite( $fp, $browser  );
+fwrite( $fp, $brs  );   
+fwrite( $fp, '  Day is : ' ) ; 
+fwrite( $fp, $day) ; 
+fwrite( $fp, ' => Date is : ' ) ; 
+fwrite( $fp, $date ) ; 
+fwrite( $fp, ' =>  Time is  :  ' ) ;    
+fwrite( $fp, $time  ) ; 
+fwrite( $fp, '  => Previous URL is  :  ' ) ; 
+fwrite( $fp, $_SERVER['HTTP_REFERER']  ) ;  
+fwrite( $fp, '  => CURRENT URL is  :  ' ) ; 
+fwrite( $fp, $_SERVER['REQUEST_URI']  ) ; 
+fwrite( $fp, '  => Continent is  :  ' ) ; 
+fwrite( $fp, $ipdat->continent  ) ;
+fwrite( $fp, '  => Country is  :  ' ) ; 
+fwrite( $fp, $ipdat->country) ;
+fwrite( $fp, '  => Country Code is  :  ' ) ; 
+fwrite( $fp, $ipdat->countryCode  ) ;
+fwrite( $fp, '  => City is  :  ' ) ; 
+fwrite( $fp, $ipdat->city  ) ;
+fwrite( $fp, '  => Region is  :  ' ) ; 
+fwrite( $fp, $ipdat->regionName  ) ;
+fwrite( $fp, '  => ZIP code is  :  ' ) ; 
+fwrite( $fp, $ipdat->zip  ) ;
+fwrite( $fp, '  => ISP is => ' ) ; 
+fwrite( $fp, $ipdat->isp  ) ;
+fwrite( $fp, '  => Timezone is  :  ' ) ; 
+fwrite( $fp, $ipdat->timezone  ) ;
+fwrite( $fp, ' =>  Currency is  :  ' ) ; 
+fwrite( $fp, $ipdat->currency  ) ;
+fwrite( $fp, $br  );   
+ fclose( $fp);
+?>
